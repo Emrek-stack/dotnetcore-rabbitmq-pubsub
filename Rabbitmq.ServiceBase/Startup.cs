3236dd5d62849b7279f435abab698d68c56f9ef9
Emre Karahan <emre.karahan@boynergrup.com>
2018-03-14 17:51:39 +0300

    first

---
+++ Rabbitmq.ServiceBase/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace Rabbitmq.ServiceBase
+{
+    public class Startup
+    {
+        IConfigurationRoot Configuration { get; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging();
+            services.AddSingleton<IConfigurationRoot>(Configuration);
+            services.AddSingleton<IMyService, MyService>();
+        }
+    }
+}
