eb1440ab4de822712090f3370feeecce2b13d2b5
Emre Karahan <emre.karahan@boynergrup.com>
2018-03-14 17:48:14 +0300

    first

---
+++ Rabbitmq.Core/Model/RabbitOption.cs
@@ -0,0 +1,10 @@
+﻿namespace Rabbitmq.Core.Model
+{
+    public class RabbitOption
+    {
+        public string Hostname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
